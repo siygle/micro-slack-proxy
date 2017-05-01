9e5d5822682d5e0a767f9cb0f185c9464ba79b24
Ferrari Lee <shiyung@gmail.com>
2017-05-01 15:59:52 +0800

    1. remove now-logs and use native support from zeit 2. fixed respond message not appear correctly 3. cleanup

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-require('now-logs')(process.env.NOW_LOGS_KEY)
 const { json, send } = require('micro')
 const parse = require('urlencoded-body-parser')
 const contentType = require('content-type')
 const fetch = require('node-fetch')
 const qs = require('querystring')
+const debug = require('debug')('micro-slack-proxy')
 
 const PROXY_HOST = process.env.HOST
 
@@ -37,19 +37,25 @@ async function _prepareProxy (type, req) {
 }
 
 module.exports = async (req, res) => {
-  const reqContent = contentType.parse(req)
+  let jsData, resData, resBuffer, resText
 
-  let jsData = await _prepareProxy(reqContent.type, req)
-  if (!jsData) {
-    console.error('invalid data format')
-    return ''
+  try {
+    const reqContent = contentType.parse(req)
+    jsData = await _prepareProxy(reqContent.type, req)
+    if (!jsData) {
+      console.error('invalid data format')
+      return ''
+    }
+  } catch (e) {
+    console.log('Did not include content type header')    
   }
 
-  let resData
   const PROXY = `${PROXY_HOST}${req.url}`
+  debug('proxy to: [%s]%s - %o', req.method, PROXY, jsData)
   switch (String(req.method).toUpperCase()) {
     case 'GET':
       resData = await fetch(PROXY)
+      resText = await resData.text()
       break
     case 'POST':
       resData = await fetch(PROXY, { 
@@ -57,6 +63,7 @@ module.exports = async (req, res) => {
         body: jsData.body,
         headers: jsData.header
       })
+      resBuffer = await resData.buffer()
       break
     case 'PUT':
       resData = await fetch(PROXY, { 
@@ -64,22 +71,28 @@ module.exports = async (req, res) => {
         body: jsData.body,
         headers: jsData.header
       })
+      resBuffer = await resData.buffer()
       break
     case 'DELETE':
       resData = await fetch(PROXY, { method: 'DELETE' })
+      resBuffer = await resData.buffer()
       break
     default:
       console.error(`Not support request method: ${req.method}`)
   }
 
-  const resBuffer = await resData.buffer()
-  if (resBuffer.length > 0) {
+  if (resBuffer && resBuffer.length > 0) {
+    let bufferContent = resBuffer.toString()
+    debug('res buffer: %s', bufferContent)
     try {
-      const resJson = JSON.parse(resBuffer.toString())
+      const resJson = JSON.parse(bufferContent)
       return send(res, resData.status, resJson)
     } catch (e) {
-      return send(res, resData.status)
+      return send(res, resData.status, bufferContent)
     }
+  } else if (resText && resText.length > 0) {
+    debug('res buffer: %s', resText)
+    return send(res, resData.status, resText)
   } else {
     return send(res, resData.status)
   }
